55e78f3fccc3d01f46a9b453085ef999a1f910db
Minsoo, Kim <thouuy@gmail.com>
2021-11-01 13:08:34 +0900

    Add zabbix library
    Add zabbix library

---
+++ paasta-monitoring-portal/src/zabbix-client/history/history.go
@@ -0,0 +1,57 @@
+package history
+
+import (
+	"github.com/cavaliercoder/go-zabbix"
+	"log"
+	"zabbix-client/utils"
+)
+
+
+var isDebug bool
+
+func init() {
+	isDebug = false
+}
+
+/**
+getHistory
+	parameters
+		- itemId (string) : 조회할 item의 ID
+		- offset (int)    : 불러올 데이터 개수
+		- type (int)      : 데이터 타입
+*/
+func GetHistory(session *zabbix.Session, params map[string]interface{}) []zabbix.History {
+	var historyParams zabbix.HistoryGetParams
+
+	itemArr := make([]string, 1)
+	itemId, ok := params["itemId"].(string)
+	if ok {
+		itemArr[0] = itemId
+		historyParams.ItemIDs = itemArr
+	}
+
+	offset, ok := params["offset"].(int)
+	if ok {
+		historyParams.ResultLimit = offset
+	} else {
+		historyParams.ResultLimit = 10
+	}
+
+	itemType, ok := params["itemType"].(int)
+	if ok {
+		historyParams.History = itemType
+	}
+
+	historyParams.SortField = zabbix.SelectFields{"clock"}
+	historyParams.SortOrder = "DESC"
+
+	result, err := session.GetHistories(historyParams)
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+
+	if isDebug {
+		utils.PrintJson(result)
+	}
+	return result
+}
---
+++ paasta-monitoring-portal/src/zabbix-client/host/host.go
@@ -0,0 +1,51 @@
+package host
+
+import (
+	"log"
+
+	"github.com/cavaliercoder/go-zabbix"
+
+	"zabbix-client/utils"
+)
+
+var isDebug bool
+
+func init() {
+	isDebug = false
+}
+
+/**
+	GetHostList
+		Parameters
+			- filter (map[string]interface{}) : 필터 맵
+			- groupIds ([]string) : 호스트가 속한 그룹의 ID 배열
+			- hostIds ([]string) : 호스트 ID 배열
+ */
+func GetHostList(session *zabbix.Session, params map[string]interface{}) []zabbix.Host {
+	var hostParams zabbix.HostGetParams
+
+	filterMap, ok := params["filter"]
+	if ok {
+		hostParams.Filter = filterMap.(map[string]interface{})
+	}
+
+	groupIds, ok := params["groupIds"]
+	if ok {
+		hostParams.GroupIDs = groupIds.([]string)
+	}
+
+
+	//hostParams.SelectItems = zabbix.SelectFields{"name", "lastvalue", "units", "itemid", "lastclock", "value_type"}
+	hostParams.SelectInterfaces = zabbix.SelectFields{"ip"}
+	hostParams.OutputFields = "extend"
+	result, err := session.GetHosts(hostParams)
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+
+	if isDebug {
+		utils.PrintJson(result)
+	}
+
+	return result
+}
