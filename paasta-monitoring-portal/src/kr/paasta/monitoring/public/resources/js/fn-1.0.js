cb0a550f660466c7cbd0ae7920a7c80cffba4b07
YoongJwe <yoongjwe@gmail.com>
2021-11-04 09:36:01 +0900

    단위 변환 기능 추가
    IaaS 정보 조회시 크기에 따른 단위 변환 기능 추가

--- paasta-monitoring-portal/src/kr/paasta/monitoring/public/resources/js/fn-1.0.js
+++ paasta-monitoring-portal/src/kr/paasta/monitoring/public/resources/js/fn-1.0.js
@@ -14,7 +14,7 @@ const fnComm = {
 		document.querySelector('.timeSetting').addEventListener('click', () => {
 			document.querySelector('.timePop').classList.toggle('on', true);
 		});
-		
+
 		document.querySelector('.timePop > a').addEventListener('click', () => {
 			document.querySelector('.timePop').classList.toggle('on', false);
 		});
@@ -51,7 +51,7 @@ const fnComm = {
 				};
 			};
 		};
-		
+
 		request.send(`{"username":"${user}","password":"${pw}"}`);
 	},
 
@@ -65,7 +65,7 @@ const fnComm = {
 			console.log(request);
 
 			var tokenArray = request.getAllResponseHeaders().toLowerCase().split('\n');
-			
+
 			for(let value of tokenArray){
 				if(value.indexOf('x-xsrf-token') != -1){
 					console.log(value.split(': ')[1]);
@@ -84,7 +84,7 @@ const fnComm = {
 	alarmCount(type){
 		document.querySelector('.outBtn strong').innerHTML = sessionStorage.getItem('user');
 		document.querySelector('.outBtn span').innerHTML = sessionStorage.getItem('mail');
-		
+
 		// 오늘 날짜
 		var today = new Date();
 		var dd = today.getDate();
@@ -95,7 +95,7 @@ const fnComm = {
 		if(dd < 10) dd = '0'+dd;
 		if(mm < 10) mm = '0'+mm;
 		if(nn < 10) nn = '0'+nn;
-		
+
 		// 검색 Default set
 		var form = `${yyyy}-${nn}-${dd}`;
 		var to = `${yyyy}-${mm}-${dd}`;
@@ -112,9 +112,9 @@ const fnComm = {
 				url = `${fnComm.url}saas/app/application/alarmCount?searchDateFrom=${form}&searchDateTo=${to}`;
 			break;
 		};
-		
+
 		var request = new XMLHttpRequest();
-		
+
 		try {
 			request.open('GET', url, false);
 			request.setRequestHeader('X-XSRF-TOKEN', sessionStorage.getItem('token'));
@@ -125,7 +125,7 @@ const fnComm = {
 						if(JSON.parse(request.responseText).totalCnt > 0){
 							document.querySelector('.alarmView').classList.toggle('on', true);
 							document.querySelector('.alarmView span').innerHTML = JSON.parse(request.responseText).totalCnt;
-							
+
 							document.querySelector('.alarmView span').addEventListener('click', () => {
 								location.href = 'alarm_status.html';
 							});
@@ -153,7 +153,7 @@ const fnComm = {
 					document.querySelector(`.global .CaaS`).style.display = 'inline-block';
 				}
 			};
-		
+
 			// logout 이벤트
 			document.querySelector('.logout').addEventListener('click', (e) => {
 				sessionStorage.clear();
@@ -181,7 +181,7 @@ const fnComm = {
 		document.querySelector('.timeSetting').addEventListener('click', (e) => {
 			document.querySelector('.timePop').classList.toggle('on');
 		}, false);
-		
+
 		document.querySelector('.timePop .close').addEventListener('click', (e) => {
 			document.querySelector('.timePop').classList.toggle('on');
 		}, false);
@@ -233,7 +233,7 @@ const fnComm = {
 		var request = new XMLHttpRequest();
 		request.open(method, url);
 		request.setRequestHeader('X-XSRF-TOKEN', sessionStorage.getItem('token'));
-		
+
 		console.log(sessionStorage.getItem('token'));
 
 		request.onreadystatechange = () => {
@@ -273,6 +273,34 @@ const fnComm = {
     numberComma(digit, number){
         return number.toFixed(digit).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     },
+
+	convertUnits(size, unit){
+		var convertedSize, convertedUnit;
+		if (unit == "MB") {
+			if (size >= 1048576) {
+				convertedSize = size / 1048576;
+				convertedUnit = "TB";
+			} else if (size >= 1024) {
+				convertedSize = size / 1024;
+				convertedUnit = "GB";
+			} else {
+				convertedSize = size;
+				convertedUnit = "MB";
+			}
+		} else if (unit == "GB") {
+			if (size >= 1048576) {
+				convertedSize = size / 1048576;
+				convertedUnit = "PB";
+			} else if (size >= 1024) {
+				convertedSize = size / 1024;
+				convertedUnit = "TB";
+			} else {
+				convertedSize = size;
+				convertedUnit = "GB";
+			};
+		};
+		return {convertedSize, convertedUnit};
+	},
 
 	/////////////////////////////////////////////////////////////////////////////////////
 	// 날짜 컨버팅 - unixTime(time)
@@ -329,7 +357,7 @@ const fnComm = {
 
 		document.getElementById('alertPop').querySelector('.closed').addEventListener('click', (e) => {
 			if(callback) callback();
-			
+
 			document.body.removeChild(document.getElementById('alertPop'));
 		}, false);
 	},
@@ -359,7 +387,7 @@ const fnComm = {
     countUp(el, num) {
 		var cnt = 0;
 		var dif = 0;
-		
+
 		var thisID = setInterval(function(){
 			if(cnt < num){
 				dif = num - cnt;
@@ -475,7 +503,7 @@ const fnComm = {
 		let detailTime = [];
 		let detailData = [];
 		let chartColor;
-		
+
 		data.forEach((load, idx) => {
 			if(idx === 0){
 				detailTime.push('time');
@@ -499,7 +527,7 @@ const fnComm = {
 		});
 
 		yPos = Math.ceil((yPos / 10)) * 10;
-		
+
 		var dataType = [];
 		dataType.push(detailTime);
 		for (var i = 0; i <= cnt; i++) {
@@ -565,7 +593,7 @@ const fnComm = {
 			}
 		});
 	},
-	
+
 	// Gauge 차트 //////////////////////////////////////////////////////////////////////////
 	gaugeChart(data, category, target){
 		var chart = c3.generate({
