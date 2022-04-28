03e112ee327158ec9d2c0bfce121083d8851f1cd
YoongJwe <yoongjwe@gmail.com>
2022-04-28 16:21:33 +0900

    Add BasicResponseForm for JSON response
    . Add BasicResponseForm struct type
    . Add Respond func (returns common response form)
    . Refine some contents of message fields

--- paasta-monitoring-api/src/kr/paasta/monitoring-api/controllers/api/v1/token.go
+++ paasta-monitoring-api/src/kr/paasta/monitoring-api/controllers/api/v1/token.go
@@ -1,69 +1,64 @@
 package v1
 
 import (
-	"GoEchoProject/connections"
-	"GoEchoProject/models"
-	v1service "GoEchoProject/services/api/v1"
-	"github.com/go-redis/redis/v7"
-	"github.com/jinzhu/gorm"
-	"github.com/labstack/echo/v4"
-	"net/http"
+    "GoEchoProject/apiHelpers"
+    "GoEchoProject/connections"
+    "GoEchoProject/models"
+    v1service "GoEchoProject/services/api/v1"
+    "github.com/go-redis/redis/v7"
+    "github.com/jinzhu/gorm"
+    "github.com/labstack/echo/v4"
+    "net/http"
 )
 
 type TokenController struct {
-	DbInfo    *gorm.DB
-	RedisInfo *redis.Client
+    DbInfo    *gorm.DB
+    RedisInfo *redis.Client
 }
 
 func GetTokenController(conn connections.Connections) *TokenController {
-	return &TokenController{
-		DbInfo:    conn.DbInfo,
-		RedisInfo: conn.RedisInfo,
-	}
+    return &TokenController{
+        DbInfo:    conn.DbInfo,
+        RedisInfo: conn.RedisInfo,
+    }
 }
 
 func (a *TokenController) CreateToken(c echo.Context) (err error) {
 
-	/* Request Body Data를 매핑한다. */
-	var apiRequest models.UserInfo // -> &추가
-	if err = c.Bind(&apiRequest); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, "Invalid json provided")
-	}
+    /* Request Body Data를 매핑한다. */
+    var apiRequest models.UserInfo // -> &추가
+    if err = c.Bind(&apiRequest); err != nil {
+        return echo.NewHTTPError(http.StatusBadRequest, "Invalid json provided")
+    }
 
-	// Authentication의 CreateToken 발급을 호출한다.
-	tokenDetails, err := v1service.GetTokenService(a.DbInfo, a.RedisInfo).CreateToken(apiRequest, c)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	}
+    // Authentication의 CreateToken 발급을 호출한다.
+    tokenDetails, err := v1service.GetTokenService(a.DbInfo, a.RedisInfo).CreateToken(apiRequest, c)
+    if err != nil {
+        apiHelpers.Respond(c, http.StatusBadRequest, "Cannot create token", err.Error())
+        return err
+    }
 
-	// TokenDetails로 토근 정보를 전달한다. (AccessToken, RefreshToken, AccessUuid, RefreshUuid, AtExpires, RtExpires)
-	// JSON 같은 경우 Unhandled error에 대한 처리를 필요로 한다.
-	err = c.JSON(http.StatusOK, tokenDetails)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-	}
-	return nil
+    // TokenDetails로 토근 정보를 전달한다. (AccessToken, RefreshToken, AccessUuid, RefreshUuid, AtExpires, RtExpires)
+    apiHelpers.Respond(c, http.StatusOK, "Success to create token", tokenDetails)
+    return nil
 }
 
 func (a *TokenController) RefreshToken(c echo.Context) (err error) {
 
-	/* Request Body Data를 매핑한다.  */
-	var apiRequest models.TokenDetails // -> &추가
-	if err = c.Bind(&apiRequest); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, "Invalid json provided")
-	}
+    /* Request Body Data를 매핑한다.  */
+    var apiRequest models.TokenDetails // -> &추가
+    if err = c.Bind(&apiRequest); err != nil {
+        return echo.NewHTTPError(http.StatusBadRequest, "Invalid json provided")
+    }
 
-	// Authentication의 RefreshToken 발급을 호출한다.
-	tokenDetails, err := v1service.GetTokenService(a.DbInfo, a.RedisInfo).RefreshToken(apiRequest, c)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	}
+    // Authentication의 RefreshToken 발급을 호출한다.
+    tokenDetails, err := v1service.GetTokenService(a.DbInfo, a.RedisInfo).RefreshToken(apiRequest, c)
+    if err != nil {
+        apiHelpers.Respond(c, http.StatusBadRequest, "Cannot refresh token", err.Error())
+        return err
+    }
 
-	// TokenDetails로 토근 정보를 전달한다. (AccessToken, RefreshToken, AccessUuid, RefreshUuid, AtExpires, RtExpires)
-	// JSON 같은 경우 Unhandled error에 대한 처리를 필요로 한다.
-	err = c.JSON(http.StatusOK, tokenDetails)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-	}
-	return nil
+    // TokenDetails로 토근 정보를 전달한다. (AccessToken, RefreshToken, AccessUuid, RefreshUuid, AtExpires, RtExpires) .
+    apiHelpers.Respond(c, http.StatusOK, "Success to refresh token", tokenDetails)
+    return nil
 }
--- paasta-monitoring-api/src/kr/paasta/monitoring-api/controllers/api/v1/user.go
+++ paasta-monitoring-api/src/kr/paasta/monitoring-api/controllers/api/v1/user.go
@@ -1,41 +1,42 @@
 package v1
 
 import (
-	"GoEchoProject/connections"
-	"GoEchoProject/models"
-	v1service "GoEchoProject/services/api/v1"
-	"github.com/jinzhu/gorm"
-	"github.com/labstack/echo/v4"
-	"net/http"
+    "GoEchoProject/apiHelpers"
+    "GoEchoProject/connections"
+    "GoEchoProject/models"
+    v1service "GoEchoProject/services/api/v1"
+    "github.com/jinzhu/gorm"
+    "github.com/labstack/echo/v4"
+    "net/http"
 )
 
 type UserController struct {
-	DbInfo *gorm.DB
+    DbInfo *gorm.DB
 }
 
 func GetUserController(conn connections.Connections) *UserController {
-	return &UserController{
-		DbInfo: conn.DbInfo,
-	}
+    return &UserController{
+        DbInfo: conn.DbInfo,
+    }
 }
 
 func (a *UserController) GetUsers(c echo.Context) (err error) {
-	/* Request Body Data Mapping */
-	var apiRequest models.UserInfo // -> &추가
-	//apiRequest := new(models.UserInfo) // &제거
-	if err = c.Bind(&apiRequest); err != nil {
-		c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
-		return nil
-	}
+    /* Request Body Data Mapping */
+    var apiRequest models.UserInfo // -> &추가
+    //apiRequest := new(models.UserInfo) // &제거
+    if err = c.Bind(&apiRequest); err != nil {
+        c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
+        return nil
+    }
 
-	// User의 GetUsers를 호출한다.
-	users, err := v1service.GetUserService(a.DbInfo).GetUsers(apiRequest, c)
-	if err != nil {
-		c.JSON(http.StatusUnauthorized, err.Error())
-		return nil
-	}
+    // User의 GetUsers를 호출한다.
+    users, err := v1service.GetUserService(a.DbInfo).GetUsers(apiRequest, c)
+    if err != nil {
+        c.JSON(http.StatusUnauthorized, err.Error())
+        return nil
+    }
 
-	// 사용자 정보를 전달한다.
-	c.JSON(http.StatusOK, users)
-	return nil
+    // 사용자 정보를 전달한다.
+    apiHelpers.Respond(c, http.StatusOK, "Success to get all users", users)
+    return nil
 }
