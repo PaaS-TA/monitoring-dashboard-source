7d44399fcdd7312fdbbda4f10a841132bcfa4533
YoongJwe <yoongjwe@gmail.com>
2022-04-28 15:01:02 +0900

    Add BasicResponseForm for JSON response
    . Add BasicResponseForm struct type
    . Add Respond func (returns common response form)
    .  Refine some contents of message fields

--- paasta-monitoring-api/src/kr/paasta/monitoring-api/apiHelpers/Response.go
+++ paasta-monitoring-api/src/kr/paasta/monitoring-api/apiHelpers/Response.go
@@ -1,28 +1,45 @@
 package apiHelpers
 
 import (
-	"encoding/json"
-	"net/http"
+    "github.com/labstack/echo/v4"
+    "time"
 )
 
 //ResponseData structure
 type ResponseData struct {
-	Data interface{} `json:"data"`
-	Meta interface{} `json:"meta"`
+    Data interface{} `json:"data"`
+    Meta interface{} `json:"meta"`
+}
+
+type BasicResponseForm struct {
+    StatusCode   int         `json:"statusCode"`
+    Message      string      `json:"message"`
+    ResponseTime time.Time   `json:"responseTime"`
+    ResponseInfo interface{} `json:"responseInfo"`
 }
 
 // Internal Server Error Format
 func InternalErrMessage(err error) {
-	panic(err)
+    panic(err)
 }
 
 // Request & Response Error Format
 func ExternalErrMessage(status int, message string) map[string]interface{} {
-	return map[string]interface{}{"status": status, "message": message}
+    return map[string]interface{}{"status": status, "message": message}
 }
 
-//Respond returns basic response structure
-func Respond(w http.ResponseWriter, data map[string]interface{}) {
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
+func SetBasicResponseForm(statusCode int, message string, responseInfo interface{}) BasicResponseForm {
+    responseForm := BasicResponseForm{
+        StatusCode:   statusCode,
+        Message:      message,
+        ResponseTime: time.Now().Local(),
+        ResponseInfo: responseInfo,
+    }
+    return responseForm
+}
+
+//Respond returns JSON format message with basic response form
+func Respond(c echo.Context, statusCode int, message string, responseInfo ...interface{}) {
+    response := SetBasicResponseForm(statusCode, message, responseInfo)
+    c.JSON(statusCode, response)
 }
