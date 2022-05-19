bb8d2851ed3e859a0f8179971319cc630f27b406
YoongJwe <yoongjwe@gmail.com>
2022-05-19 15:22:35 +0900

    Modify some token models

--- paasta-monitoring-api/src/kr/paasta/monitoring-api/models/api/v1/token.go
+++ paasta-monitoring-api/src/kr/paasta/monitoring-api/models/api/v1/token.go
@@ -1,22 +1,21 @@
 package v1
 
 type (
-    TokenDetails struct {
-        AccessToken  string `json:"accessToken" validate:"required"`
-        RefreshToken string `json:"refreshToken" validate:"required"`
-        AccessUuid   string `json:"accessUuid"`
-        RefreshUuid  string `json:"refreshUuid"`
-        AtExpires    int64  `json:"atExpires"`
-        RtExpires    int64  `json:"rtExpires"`
-    }
+	TokenDetails struct {
+		AccessToken  string `json:"accessToken"`
+		RefreshToken string `json:"refreshToken"`
+		AccessUuid   string `json:"accessUuid"`
+		RefreshUuid  string `json:"refreshUuid"`
+		AtExpires    int64  `json:"atExpires"`
+		RtExpires    int64  `json:"rtExpires"`
+	}
 
-    CreateToken struct {
-        Username string `json:"username" example:"username" validate:"required"`
-        Password string `json:"password" example:"password" validate:"required"`
-    }
+	CreateToken struct {
+		Username string `json:"username" example:"username" validate:"required"`
+		Password string `json:"password" example:"password" validate:"required"`
+	}
 
-    RefreshToken struct {
-        AccessToken  string `json:"accessToken" example:"accessToken" validate:"required"`
-        RefreshToken string `json:"refreshToken" example:"refreshToken" validate:"required"`
-    }
+	RefreshToken struct {
+		RefreshToken string `json:"refreshToken" example:"refreshToken" validate:"required"`
+	}
 )
