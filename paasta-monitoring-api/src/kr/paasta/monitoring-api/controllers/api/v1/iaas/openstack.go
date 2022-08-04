882a560d398117cee7eee67ff686c23a30a9cf30
YoongJwe <yoongjwe@gmail.com>
2022-08-04 15:44:25 +0900

    Update Swagger Annotation
    - IaaS > OpenStack 파트 작성.

--- paasta-monitoring-api/src/kr/paasta/monitoring-api/controllers/api/v1/iaas/openstack.go
+++ paasta-monitoring-api/src/kr/paasta/monitoring-api/controllers/api/v1/iaas/openstack.go
@@ -13,67 +13,84 @@ type OpenstackController struct {
 	OpenstackProvider *gophercloud.ProviderClient
 }
 
-
 func GetOpenstackController(openstackProvider *gophercloud.ProviderClient) *OpenstackController {
 	return &OpenstackController{
 		OpenstackProvider: openstackProvider,
 	}
 }
 
-
+// GetHypervisorStatistics
+//  @tags         IaaS
+//  @Summary      오픈스택 하이퍼바이저 통계 가져오기
+//  @Description  오픈스택 하이퍼바이저 통계를 가져온다.
+//  @Accept       json
+//  @Produce      json
+//  @Success      200  {object}  apiHelpers.BasicResponseForm
+//  @Router       /api/v1/iaas/hypervisor/stats [get]
 func (controller *OpenstackController) GetHypervisorStatistics(ctx echo.Context) error {
 	results, err := service.GetOpenstackService(controller.OpenstackProvider).GetHypervisorStatistics(ctx)
 	if err != nil {
-		apiHelpers.Respond(ctx, http.StatusBadRequest, "Failed to get Hypervisor statistics.", err.Error())
+		apiHelpers.Respond(ctx, http.StatusBadRequest, "Failed to get Hypervisor Statistics.", err.Error())
 		return err
 	} else {
-		apiHelpers.Respond(ctx, http.StatusOK, "", results)
+		apiHelpers.Respond(ctx, http.StatusOK, "Succeeded to get Hypervisor Statistics.", results)
 	}
 	return nil
 }
 
-
-/**
-	하이퍼바이저 목록 (상세 정보 포함) 조회
-*/
+// GetHypervisorList
+//  @tags         IaaS
+//  @Summary      오픈스택 하이퍼바이저 리스트 가져오기
+//  @Description  오픈스택 하이퍼바이저 리스트를 가져온다.
+//  @Accept       json
+//  @Produce      json
+//  @Success      200  {object}  apiHelpers.BasicResponseForm
+//  @Router       /api/v1/iaas/hypervisor/list [get]
 func (controller *OpenstackController) GetHypervisorList(ctx echo.Context) error {
 	results, err := service.GetOpenstackService(controller.OpenstackProvider).GetHypervisorList(ctx)
 	if err != nil {
-		apiHelpers.Respond(ctx, http.StatusBadRequest, "Failed to get Hypervisor statistics.", err.Error())
+		apiHelpers.Respond(ctx, http.StatusBadRequest, "Failed to get Hypervisor List.", err.Error())
 		return err
 	} else {
-		apiHelpers.Respond(ctx, http.StatusOK, "", results)
+		apiHelpers.Respond(ctx, http.StatusOK, "Succeeded to get Hypervisor List.", results)
 	}
 	return nil
 }
 
-
-/**
-	프로젝트 목록(만) 조회
-*/
+// GetProjectList
+//  @tags         IaaS
+//  @Summary      오픈스택 프로젝트 리스트 가져오기
+//  @Description  오픈스택 프로젝트 리스트를 가져온다.
+//  @Accept       json
+//  @Produce      json
+//  @Success      200  {object}  apiHelpers.BasicResponseForm
+//  @Router       /api/v1/iaas/project/list [get]
 func (controller *OpenstackController) GetProjectList(ctx echo.Context) error {
 	results, err := service.GetOpenstackService(controller.OpenstackProvider).GetProjectList(ctx)
 	if err != nil {
-		apiHelpers.Respond(ctx, http.StatusBadRequest, "Failed to get Hypervisor statistics.", err.Error())
+		apiHelpers.Respond(ctx, http.StatusBadRequest, "Failed to get Project List.", err.Error())
 		return err
 	} else {
-		apiHelpers.Respond(ctx, http.StatusOK, "", results)
+		apiHelpers.Respond(ctx, http.StatusOK, "Succeeded to get Project List.", results)
 	}
 	return nil
 }
 
-
-/**
-프로젝트(테넌트) 목록과 usage 정보를 조회
-	- 프로젝트에 속한 인스턴스 목록과 usage 조회도 가능하나 현재는 비활성화 되어 있음
-*/
+// GetProjectUsage
+//  @tags         IaaS
+//  @Summary      오픈스택 프로젝트 사용량 가져오기
+//  @Description  오픈스택 프로젝트 사용량을 가져온다.
+//  @Accept       json
+//  @Produce      json
+//  @Success      200  {object}  apiHelpers.BasicResponseForm{responseInfo=[]usage.TenantUsage}
+//  @Router       /api/v1/iaas/instance/usage/list [get]
 func (controller *OpenstackController) GetProjectUsage(ctx echo.Context) error {
 	results, err := service.GetOpenstackService(controller.OpenstackProvider).RetrieveTenantUsage(ctx)
 	if err != nil {
-		apiHelpers.Respond(ctx, http.StatusBadRequest, "Failed to get Hypervisor statistics.", err.Error())
+		apiHelpers.Respond(ctx, http.StatusBadRequest, "Failed to get Project Usage.", err.Error())
 		return err
 	} else {
-		apiHelpers.Respond(ctx, http.StatusOK, "", results)
+		apiHelpers.Respond(ctx, http.StatusOK, "Succeeded to get Project Usage.", results)
 	}
 	return nil
 }
