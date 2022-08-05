2de0f721a53999068db57efa2be92d94496962b8
YoongJwe <yoongjwe@gmail.com>
2022-08-05 17:22:43 +0900

    Update Swagger Annotation
    - SaaS > Pinpoint 파트

--- paasta-monitoring-api/src/kr/paasta/monitoring-api/controllers/api/v1/saas/pinpoint.go
+++ paasta-monitoring-api/src/kr/paasta/monitoring-api/controllers/api/v1/saas/pinpoint.go
@@ -12,33 +12,49 @@ type PinpointController struct {
 	SaaS models.SaaS
 }
 
-
 func GetPinpointController(saas models.SaaS) *PinpointController {
 	return &PinpointController{
 		SaaS: saas,
 	}
 }
 
-
+// GetAgentList
+//  @Tags         SaaS
+//  @Summary      핀포인트 모니터링 에이전트 리스트 가져오기
+//  @Description  핀포인트 모니터링 에이전트 리스트를 가져온다.
+//  @Accept       json
+//  @Produce      json
+//  @Success      200  {object}  apiHelpers.BasicResponseForm
+//  @Router       /api/v1/saas/pinpoint/getAgentList [get]
 func (controller *PinpointController) GetAgentList(ctx echo.Context) error {
 	result, err := service.GetPinpointService(controller.SaaS).GetAgentList(ctx)
 	if err != nil {
-		apiHelpers.Respond(ctx, http.StatusBadRequest, "Failed to get Hypervisor statistics.", err.Error())
+		apiHelpers.Respond(ctx, http.StatusBadRequest, "Failed to get Pinpoint Agent List.", err.Error())
 		return err
 	} else {
-		apiHelpers.Respond(ctx, http.StatusOK, "", result)
+		apiHelpers.Respond(ctx, http.StatusOK, "Succeeded to get Pinpoint Agent List.", result)
 	}
 	return nil
 }
 
-
+// GetAgentStat
+//  @Tags         SaaS
+//  @Summary      핀포인트 모니터링 에이전트 통계 가져오기
+//  @Description  핀포인트 모니터링 에이전트 통계를 가져온다.
+//  @Accept       json
+//  @Produce      json
+//  @Param        chartType  path      string  true  "차트 타입을 주입한다."                    enums(cpuLoad, jvmGc, activeTrace, responseTime)
+//  @Param        agentId    query     string  true  "에이전트 아이디를 주입한다."                 example(3149075187)
+//  @Param        period     query     string  true  "현재를 조회하고 싶은 기간을 주입한다. 단위는 분(m),  시(h),  일(d)  단위를  사용한다."        example(1m)
+//  @Success      200        {object}  apiHelpers.BasicResponseForm
+//  @Router       /api/v1/saas/pinpoint/{chartType}/getAgentStat [get]
 func (controller *PinpointController) GetAgentStat(ctx echo.Context) error {
 	result, err := service.GetPinpointService(controller.SaaS).GetAgentStat(ctx)
 	if err != nil {
-		apiHelpers.Respond(ctx, http.StatusBadRequest, "Failed to get Hypervisor statistics.", err.Error())
+		apiHelpers.Respond(ctx, http.StatusBadRequest, "Failed to get Pinpoint Agent Stat.", err.Error())
 		return err
 	} else {
-		apiHelpers.Respond(ctx, http.StatusOK, "", result)
+		apiHelpers.Respond(ctx, http.StatusOK, "Succeeded to get Pinpoint Agent Stat", result)
 	}
 	return nil
 }
