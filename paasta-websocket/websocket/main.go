351f7965b855ce99cf4fd68ab9b0e887a6196688
jihyeonnnn <seoji945@gmail.com>
2022-12-07 17:34:36 +0900

    modify files

--- paasta-websocket/websocket/main.go
+++ paasta-websocket/websocket/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 	"sync"
+	"time"
 )
+
+type JsonData struct {
+	Type  string `json:"type"`
+	Time  string `json:"time"`
+	Usage string `json:"usage"`
+	Index int    `json:"Index"`
+}
+
+type JsonDataArray struct {
+	array []JsonData
+}
 
 type Client struct {
 	conn      *websocket.Conn
@@ -25,7 +38,7 @@ var upgrader = websocket.Upgrader{
 }
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("static")))
+	http.Handle("/", http.FileServer(http.Dir("static/public")))
 	http.HandleFunc("/ws", socketHandler)
 
 	port := "8080"
@@ -36,8 +49,37 @@ func main() {
 }
 
 func (c *Client) readFunc() {
-	for {
-		_, message, err := c.conn.ReadMessage() /*클라이언트에서 받은 websocket메세지 ReadMessage메소드로 읽음.*/
+
+	JsonData1 := JsonData{"cpu", "1669892640", "2", 0}
+	JsonData2 := JsonData{"cpu", "1669892700", "3", 1}
+	JsonData3 := JsonData{"cpu", "1669892760", "4", 2}
+	JsonData4 := JsonData{"cpu", "1669892820", "5", 3}
+	JsonData5 := JsonData{"cpu", "1669892880", "3", 4}
+	JsonData6 := JsonData{"cpu", "1669892940", "10", 5}
+	JsonData7 := JsonData{"cpu", "1669893000", "2", 6}
+	JsonData8 := JsonData{"cpu", "1669893060", "5", 7}
+	JsonData9 := JsonData{"cpu", "1669893120", "1", 8}
+	JsonData10 := JsonData{"cpu", "1669893180", "4", 9}
+
+	JsonDataArray := []JsonData{
+		JsonData1, JsonData2, JsonData3,
+		JsonData4, JsonData5, JsonData6,
+		JsonData7, JsonData8, JsonData9, JsonData10}
+
+	for i := 0; i <= len(JsonDataArray); i++ {
+		b, _ := json.Marshal(JsonDataArray)
+		c.broadcast <- b
+	}
+
+	/*for _, data := range JsonDataArray {
+		b, _ := json.Marshal(data)
+		fmt.Println("read: ", string(b))
+
+		c.broadcast <- b
+	}*/
+
+	/*for {
+		_, message, err := c.conn.ReadMessage() 클라이언트에서 받은 websocket메세지 ReadMessage메소드로 읽음.
 
 		fmt.Println("read" + string(message))
 
@@ -45,9 +87,8 @@ func (c *Client) readFunc() {
 			log.Printf("conn.ReadMessage: %v", err)
 			break
 		}
-		c.broadcast <- message /*broadcast채널로 대입*/
-
-	}
+		c.broadcast <- message broadcast채널로 대입
+	}*/
 	wg.Done()
 }
 
@@ -58,14 +99,12 @@ func (c *Client) writeFunc() {
 	}()
 	for {
 		select {
-		case message, ok := <-c.broadcast: /*broadcast채널에서 데이터 꺼내기*/
-			fmt.Println("write" + string(message))
+		case message := <-c.broadcast: /*broadcast채널에서 데이터 꺼내기*/
 
-			if !ok {
-				// The hub closed the channel.
-				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
-				return
-			}
+			ticker := time.NewTicker(time.Millisecond * 3000)
+
+			time.Sleep(time.Millisecond * 1600)
+			ticker.Stop()
 
 			w, err := c.conn.NextWriter(websocket.TextMessage)
 			if err != nil {
@@ -73,14 +112,12 @@ func (c *Client) writeFunc() {
 			}
 			w.Write(message)
 
-			n := len(c.broadcast)
+			fmt.Println("write: " + string(message))
+
+			/*n := len(c.broadcast)
 			for i := 0; i < n; i++ {
 				w.Write(<-c.broadcast)
-			}
-
-			if err := w.Close(); err != nil {
-				return
-			}
+			}*/
 
 		}
 	}
